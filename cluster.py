abda683f5b1ab2dabce38c2e739f7ad6d9eb529f
Parsa <parsa@Parsas-MacBook-Air.local>
2022-01-22 14:49:33 -0500

    added utility functions

--- cluster.py
+++ cluster.py
@@ -27,3 +27,69 @@ except:
         exit()
 MAP_PATH = '/etc/TD_map/neighbors.map'
 
+
+#########################
+#check grasp
+#########################
+try:
+    graspi.checkrun
+except:
+    #not running under ASA loader
+    graspi.tprint("========================")
+    graspi.tprint("ASA server is starting up.")
+    graspi.tprint("========================")≠
+
+
+#########################
+# utility function for reading info from map; 
+# later this info will be received from ACP
+#########################
+def readmap(path):
+    file = open(path)
+    l = file.readlines()
+    l = [int(item) for item in l]
+    return l[0], l[1:]
+
+
+#########################
+# utility print function
+#########################
+def mprint(msg):
+    print("\n#######################")
+    print(msg)
+    print("#######################\n")
+
+
+#########################
+#Registering ASA 
+#########################
+def ASA_REG(name):
+    mprint("registering asa and objective")
+    err, asa_handle = graspi.register_asa(name)
+    if not err:
+        mprint("ASA registered successfully")
+        return err, asa_handle
+    else:
+        mprint("Cannot register ASA:\n\t" + graspi.etext[err])
+        mprint("exiting now.")
+
+
+#########################
+#Registering objectives
+#########################
+def OBJ_REG(name, value, neg, synch, loop_count, ASA):
+    obj = graspi.objective(name)
+    obj.value = value
+    obj.neg = neg
+    obj.synch = synch
+    obj.loop_count = loop_count
+    err = graspi.register_obj(ASA, obj)
+    if not err:
+        mprint("Objective registered successfully")
+    else:
+        mprint("Cannot register Objective:\n\t"+ graspi.etext[err])
+        mprint("exiting now.")
+    return obj, err
+        
+def TAG_OBJ(obj, ASA):
+    return graspi.tagged_objective(obj, ASA)

1539605b8bad12f4e5e549b7eeac3e37e3623851
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-01-26 10:10:37 -0500

    added flooding and listening

--- node.py
+++ node.py
@@ -2,28 +2,28 @@ from time import sleep
 from cluster import *
 import random
 import threading
-HEAD = False
+# HEAD = False
 NODE_ID, NEIGHBORS = readmap(MAP_PATH)
-CLUSTER = False
-CLUSTER_SET = []
+# CLUSTER = False
+# CLUSTER_SET = []
 
-'''
-each time node receives an weight from neighbors
-the update will be saved in this dict.
-the values in dict are initialized to False.
-'''
-RCV_NEIGHBORS = dict((RCV_NEIGHBORS,False) for RCV_NEIGHBORS in NEIGHBORS)
+# '''
+# each time node receives an weight from neighbors
+# the update will be saved in this dict.
+# the values in dict are initialized to False.
+# '''
+RCV_NEIGHBORS = dict((RCV_NEIGHBORS,0) for RCV_NEIGHBORS in NEIGHBORS)
 
-'''
-received roles are stored here
-* join(u, v) node u joins v
-* join (v, v) node v is the cluster head  = ch(v)
-'''
-RCV_ROLES = dict((tmp,False) for tmp in NEIGHBORS)
+# '''
+# received roles are stored here
+# * join(u, v) node u joins v
+# * join (v, v) node v is the cluster head  = ch(v)
+# '''
+# RCV_ROLES = dict((tmp,False) for tmp in NEIGHBORS)
 
-'''
-registering ASA named cluster
-'''
+# '''
+# registering ASA named cluster
+# '''
 cluster = ASA_REG("cluster")
 
 
@@ -59,9 +59,10 @@ class flooder(threading.Thread):
         while True:
             err = graspi.flood(self.tagged.source, 59000, [graspi.tagged_objective(self.obj.objective, None)])
             sleep(5)
+        return err
 
 node_info_flooder = flooder(tagged).start()
-#TODO stop
+# #TODO stop
 
 
 neighbor_weight = dict((tmp,None) for tmp in NEIGHBORS)
@@ -95,8 +96,8 @@ create a thread for each neighbor
 #                 mprint("can't get weight from {}".format(
 #                                         self.tagged.objective.name))
                 
-#TODO add loop count
-#it stops by itself.
+# #TODO add loop count
+# #it stops by itself.
 def listener(tagged):
     while True:
             err, result = graspi.synchronize(
@@ -123,74 +124,74 @@ for i in threads:
 for i in threads:
     threads[i].start()
 
-'''
-Update messages can be:
-    * join(u, v) where u is the neighbor ID and v is the node they're joining
-    * head(v) where v is the neighbor and announces itself as clusterhead
-'''
+# '''
+# Update messages can be:
+#     * join(u, v) where u is the neighbor ID and v is the node they're joining
+#     * head(v) where v is the neighbor and announces itself as clusterhead
+# '''
 
-role = TAG_OBJ(OBJ_REG(NODE_ID, None, False, True, 10, cluster), cluster)
+# role = TAG_OBJ(OBJ_REG(NODE_ID, None, False, True, 10, cluster), cluster)
 
-# def send_role():
+# # def send_role():
+# #     while True:
+# #         err = 
+
+# #TODO use enum
+# #if ndoe_ID and id are same, then it's ch(node_ID)
+# #ow its joining id
+# def set_role(id):
+#     if id == NODE_ID:
+#         return [NODE_ID, NODE_ID]
+#     else:
+#         return [NODE_ID, id]
+    
+# def send_role(id):
+#     role.objective.value = set_role(id)
 #     while True:
-#         err = 
+#         err = graspi.flood(role.source, 59000, [graspi.tagged_objective(role.objective, None)])
+#         sleep(5)
+# send_ch_thread = threading.Thread(target=send_role, args = [NODE_ID])
 
-#TODO use enum
-#if ndoe_ID and id are same, then it's ch(node_ID)
-#ow its joining id
-def set_role(id):
-    if id == NODE_ID:
-        return [NODE_ID, NODE_ID]
-    else:
-        return [NODE_ID, id]
+
+# def init():
+#     if WEIGHT > max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
+#         HEAD = True
+#         CLUSTER = NODE_ID
+#         CLUSTER_SET.append(NODE_ID)
+#         role.value = set_role(NODE_ID) #node is its own clusterhead
+
+#         #TODO send CH(node_ID) or join(node_ID, node_ID)
+#         send_ch_thread().start
+#     else:
+#         #run join(node_ID, neighbor_ID)
+#         pass
+# #TODO now it's like {1:[1, 2], 2[2, 2]} will be changed to {1:2, 2:2}
+# def decide():
+#     ch_list = []
+#     for i in RCV_ROLES:
+#         if RCV_ROLES[i][0] == RCV_ROLES[i][1]:
+#             ch_list.append[i]
     
-def send_role(id):
-    role.objective.value = set_role(id)
-    while True:
-        err = graspi.flood(role.source, 59000, [graspi.tagged_objective(role.objective, None)])
-        sleep(5)
-send_ch_thread = threading.Thread(target=send_role, args = [NODE_ID])
+#     to_join = None
+#     if len(ch_list)> 1:
+#         for i in RCV_NEIGHBORS:
+#             if RCV_NEIGHBORS[i] == max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
+#                 to_join = i
+#     #send join
 
 
-def init():
-    if WEIGHT > max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
-        HEAD = True
-        CLUSTER = NODE_ID
-        CLUSTER_SET.append(NODE_ID)
-        role.value = set_role(NODE_ID) #node is its own clusterhead
 
-        #TODO send CH(node_ID) or join(node_ID, node_ID)
-        send_ch_thread().start
-    else:
-        #run join(node_ID, neighbor_ID)
-        pass
-#TODO now it's like {1:[1, 2], 2[2, 2]} will be changed to {1:2, 2:2}
-def decide():
-    ch_list = []
-    for i in RCV_ROLES:
-        if RCV_ROLES[i][0] == RCV_ROLES[i][1]:
-            ch_list.append[i]
+
+# def join():
+#     max_weight = max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get)
+#     max_neighbor = None
+#     for i in RCV_NEIGHBORS:
+#         if RCV_NEIGHBORS[i] == max_weight:
+#             max_neighbor = i
+#         else:
+#             max_neighbor = NODE_ID
     
-    to_join = None
-    if len(ch_list)> 1:
-        for i in RCV_NEIGHBORS:
-            if RCV_NEIGHBORS[i] == max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
-                to_join = i
-    #send join
-
-
-
-
-def join():
-    max_weight = max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get)
-    max_neighbor = None
-    for i in RCV_NEIGHBORS:
-        if RCV_NEIGHBORS[i] == max_weight:
-            max_neighbor = i
-        else:
-            max_neighbor = NODE_ID
-    
-    #TODO flood join(node_ID, max_neighbor)
+#     #TODO flood join(node_ID, max_neighbor)
 
 
 
@@ -214,6 +215,8 @@ def join():
 
 # threading.Thread(target=changetestvalue, args=[]).start()
 # flooder(tagged).start()
+
+
 
 
 

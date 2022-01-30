d192efd7b5f86abe2d68477884f37a04d6c36cda
Parsa <parsa@Parsas-MacBook-Air.local>
2022-01-29 23:06:25 -0500

    cleaning up

--- node.py
+++ node.py
@@ -5,9 +5,6 @@ import random
 import threading
 # HEAD = False
 NODE_ID, NEIGHBORS = readmap(MAP_PATH)
-#TODO check readmap - remove cast
-# NODE_ID = str(NODE_ID)
-# NEIGHBORS = [str(tmp) for tmp in NEIGHBORS]
 CLUSTER = False
 CLUSTER_SET = []
 
@@ -58,18 +55,6 @@ role_tagged = TAG_OBJ(node_role, cluster)
 flooding weight
 stops when info from all neighbors is received
 '''
-# class flooder(threading.Thread):
-#     def __init__(self, tagged):
-#         threading.Thread.__init__(self)
-#         self.tagged = tagged
-    
-#     def run(self):
-#         while True:
-#             err = graspi.flood(self.tagged.source, 59000, [graspi.tagged_objective(self.tagged.objective, None)])
-#             sleep(5)
-#         return err
-# node_info_flooder = flooder(tagged)
-# node_info_flooder.start()
 
 def flooder(tagged, asa):
     while True:
@@ -193,10 +178,6 @@ def decide():
     while False in {key:RCV_ROLES[key+"_role"] for key in greater_weight}.values():
         mprint("waiting")
         sleep(1)
-    # greater = {key for key, value in RCV_NEIGHBORS.items() if value > WEIGHT}
-    # while False in {key: RCV_ROLES[key+"_role"] for key in greater}:
-    #     mprint("waiting for roles {}".format(RCV_ROLES))
-    #     sleep(1)
     
     mprint("for nodes with greater weight, roles have been received")
     mprint("$$$$$$$$$$$$$$$$\n{}\n$$$$$$$$$$$$$$$$".format(RCV_ROLES))
@@ -310,132 +291,3 @@ def check_ch_join():
 
 rcv_join = threading.Thread(target = check_ch_join, args=[])
 rcv_join.start()
-
-#TODO broadcast role as CH
-
-# # def send_role():
-# #     while True:
-# #         err = 
-
-# #TODO use enum
-# #if ndoe_ID and id are same, then it's ch(node_ID)
-# #ow its joining id
-# def set_role(id):
-#     if id == NODE_ID:
-#         return [NODE_ID, NODE_ID]
-#     else:
-#         return [NODE_ID, id]
-    
-# def send_role(id):
-#     role.objective.value = set_role(id)
-#     while True:
-#         err = graspi.flood(role.source, 59000, [graspi.tagged_objective(role.objective, None)])
-#         sleep(5)
-# send_ch_thread = threading.Thread(target=send_role, args = [NODE_ID])
-
-
-# def init():
-#     if WEIGHT > max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
-#         HEAD = True
-#         CLUSTER = NODE_ID
-#         CLUSTER_SET.append(NODE_ID)
-#         role.value = set_role(NODE_ID) #node is its own clusterhead
-
-#         #TODO send CH(node_ID) or join(node_ID, node_ID)
-#         send_ch_thread().start
-#     else:
-#         #run join(node_ID, neighbor_ID)
-#         pass
-# #TODO now it's like {1:[1, 2], 2[2, 2]} will be changed to {1:2, 2:2}
-# def decide():
-#     ch_list = []
-#     for i in RCV_ROLES:
-#         if RCV_ROLES[i][0] == RCV_ROLES[i][1]:
-#             ch_list.append[i]
-    
-#     to_join = None
-#     if len(ch_list)> 1:
-#         for i in RCV_NEIGHBORS:
-#             if RCV_NEIGHBORS[i] == max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get):
-#                 to_join = i
-#     #send join
-
-
-
-
-# def join():
-#     max_weight = max(RCV_NEIGHBORS, key=RCV_NEIGHBORS.get)
-#     max_neighbor = None
-#     for i in RCV_NEIGHBORS:
-#         if RCV_NEIGHBORS[i] == max_weight:
-#             max_neighbor = i
-#         else:
-#             max_neighbor = NODE_ID
-    
-#     #TODO flood join(node_ID, max_neighbor)
-
-
-
-# testobj = OBJ_REG(NODE_ID, get_node_value(), False, True, 10, cluster)
-
-# def changetestvalue(obj):
-#     while True:
-#         sleep(10)
-#         obj.value +=1
-
-# tagged = TAG_OBJ(testobj, cluster)
-
-# class flooder(threading.Thread):
-#     def __init__(self, obj):
-#         threading.Thread.__init__(self)
-#         self.obj = obj
-#     def run(self):
-#         while True:
-#             err = graspi.flood(self.obj.source, 59000, [graspi.tagged_objective(self.obj.objective, None)])
-#             time.sleep(3)
-
-# threading.Thread(target=changetestvalue, args=[]).start()
-# flooder(tagged).start()
-
-
-# ###############################################
-
-
-# from cluster import *
-# from time import sleep
-
-# def flooder(tagged, asa):
-#     while True:
-#         mprint("flooding objective {}".format(tagged.objective.name))
-#         err = graspi.flood(
-#             asa, 59000,  [graspi.tagged_objective(tagged.objective, None)]
-#         )
-#         sleep(1)
-
-# def listener(tagged, asa):
-#     while True:
-#         mprint("synchronizing obj {}".format(tagged.objective.name))
-#         err, result = graspi.synchronize(
-#             asa, tagged.objective, None, 5000
-#         )
-#         if not err:
-#             mprint("value of obj {} is being synchronized".format(tagged.objective.name))
-#             mprint("peer offered {}".format(result.value))
-#             exit()
-#         else:
-#             mprint("there is an error, {}".format(graspi.etext[err]))
-
-# NODE, NEIGHBORS = readmap(MAP_PATH)
-
-
-# err, asa = ASA_REG('asa')
-# obj, err = OBJ_REG("1", 10, False, True, 10, asa)
-# tagged = TAG_OBJ(obj, asa)
-
-# obj2, err = OBJ_REG("2", None, False, True, 10, asa)
-# tagged2 = TAG_OBJ(obj2, asa)
-
-
-# a = threading.Thread(target=flooder, args=[tagged, asa])
-# b = threading.Thread(target = listener, args=[tagged2, asa])
-# b.start()

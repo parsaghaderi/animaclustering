55c3901f44a73f185a9db260eedbe5b277514bed
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-02-04 11:16:38 -0500

    debugging

--- node_json.py
+++ node_json.py
@@ -20,7 +20,7 @@ node_json = {"name":NODE_ID,
 def json_generator():
     return json.dumps(node_json)
 
-node, err = OBJ_REG(NODE_ID, node_json, False,True,10,cluster)
+node, err = OBJ_REG(NODE_ID, json_generator(), False,True,10,cluster)
 tagged = TAG_OBJ(node,cluster)
 
 def flooder(tagged, asa):

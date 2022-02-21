af7504580cf8c744aa328cc4b864623654a41135
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-02-21 11:22:09 -0500

    testing

--- test.py
+++ test.py
@@ -296,17 +296,22 @@ if get_name() == 'Ritchie':
     # threading.Thread(target=flooder,    args = [tagged_synch]).        start()
     # threading.Thread(target=listen_neg, args = [tagged_neg])  .        start()
 
-    threading.Thread(target=synch,      args = [tagged_synch]).        start()
+    # threading.Thread(target=synch,      args = [tagged_synch]).        start()
+    obj_synch.value = 'Ritchie_synch'
+    obj_neg.value = 1
+    threading.Thread(target=flooder,    args = [tagged_synch]).        start()
 
 if get_name() == 'Gingko':
-    tagged_neg.objective.value = 50
-    threading.Thread(target=negotiate_request_side, args=[tagged_neg, old_API]).start()
+    # tagged_neg.objective.value = 50
+    # threading.Thread(target=negotiate_request_side, args=[tagged_neg, old_API]).start()
     # _, ll = graspi.discover(tagged_neg.source, tagged_neg.objective, 1000, flush = True)
     # if ll != []:
     #     for item in ll:
     #         mprint(item.locator)
     # else:
     #     mprint("discovery returned zero")
+    threading.Thread(target=synch,      args = [tagged_synch]).        start()
+
             
     
 

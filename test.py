9cb79d2313e32e975226e98509ce47a1702ca42b
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-02-21 11:27:35 -0500

    testing

--- test.py
+++ test.py
@@ -306,13 +306,14 @@ if get_name() == 'Gingko':
     # tagged_neg.objective.value = 50
     # threading.Thread(target=negotiate_request_side, args=[tagged_neg, old_API]).start()
     _, ll = graspi.discover(tagged_synch.source, tagged_synch.objective, 1000, flush = True)
+    mprint(len(ll))
     # if ll != []:
     #     for item in ll:
     #         mprint(item.locator)
     # else:
     #     mprint("discovery returned zero")
     # for item in ll:
-    threading.Thread(target=synch,      args = [tagged_synch, ll[1]]).        start()
+    #threading.Thread(target=synch,      args = [tagged_synch, ll[1]]).        start()
         
 
             

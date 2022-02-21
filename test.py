fe6bd6a42afddf8ca6d58d031bed81551069d9ed
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-02-21 10:33:24 -0500

    testing

--- test.py
+++ test.py
@@ -300,5 +300,11 @@ if get_name() == 'Ritchie':
 
 if get_name() == 'Gingko':
     tagged_neg.objective.value = 30
-    threading.Thread(target=negotiate_request_side, args=[tagged_neg, old_API]).start()
+    # threading.Thread(target=negotiate_request_side, args=[tagged_neg, old_API]).start()
+    _, ll = graspi.discover(tagged_neg.source, tagged_neg.objective, 1000, flush = True)
+    if ll != []:
+        for item in ll:
+            mprint(item.locator)
+
+    
 

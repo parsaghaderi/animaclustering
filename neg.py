6ea184c5ef5a2c261da96154011210b8d4bb58a0
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-02-24 17:07:33 -0500

    finding neighbors

---
+++ neg.py
@@ -0,0 +1,142 @@
+import os
+import random
+import threading
+import cbor
+
+try:
+    import graspi
+    _old_API = False
+
+except:
+    _old_API = True
+    import grasp as graspi
+
+# try:
+#     import graspi
+# except:
+#     print("Cannot find the RFC API module graspi.py.")
+#     print("Will run with only the basic grasp.py module.")
+#     try:
+#         _old_API = True
+#         import grasp as graspi
+#     except:
+#         print("Cannot import grasp.py")
+#         time.sleep(10)
+#         exit()
+
+try: 
+    import networkx as nx
+except:
+    print("can't import networkx; installing networkx")
+    import os
+    os.system('python3 -m pip install networkx')
+
+MAP_PATH = '/etc/TD_map/neighbors.map'
+
+#########################
+#check grasp
+#########################
+try:
+    graspi.checkrun
+except:
+    #not running under ASA loader
+    graspi.tprint("========================")
+    graspi.tprint("ASA server is starting up.")
+    graspi.tprint("========================")
+
+
+#########################
+# utility function for reading info from map; 
+# later this info will be received from ACP
+#########################
+def readmap(path):
+    file = open(path)
+    l = file.readlines()
+    l = [str(int(item)) for item in l]
+    return l[0], l[1:]
+
+
+#########################
+# utility print function
+#########################
+def mprint(msg):
+    print("\n#######################")
+    print(msg)
+    print("#######################\n")
+
+
+#########################
+#Registering ASA 
+#########################
+def ASA_REG(name):
+    mprint("registering asa and objective")
+    err, asa_handle = graspi.register_asa(name)
+    if not err:
+        mprint("ASA registered successfully")
+        return asa_handle, err
+    else:
+        mprint("Cannot register ASA:\n\t" + graspi.etext[err])
+        mprint("exiting now.")
+
+
+#########################
+#Registering objectives
+#########################
+def OBJ_REG(name, value, neg, synch, loop_count, ASA):
+    obj = graspi.objective(name)
+    obj.value = value
+    obj.neg = neg
+    obj.synch = synch
+    obj.loop_count = loop_count
+    err = graspi.register_obj(ASA, obj)
+    if not err:
+        mprint("Objective registered successfully")
+    else:
+        mprint("Cannot register Objective:\n\t"+ graspi.etext[err])
+        mprint("exiting now.")
+    return obj, err
+        
+def TAG_OBJ(obj, ASA):
+    return graspi.tagged_objective(obj, ASA)
+
+
+
+import subprocess as sp
+from time import sleep
+MAP = '/etc/TD_neighbor/locators'
+NEIGHBOR_LOCATORS = []
+
+def get_my_neighbors():
+    file = open(MAP)
+    l = file.readlines()
+    l = [item for item in l]
+    return l[0], l[1:]
+IFI, NEIGHBOR = get_my_neighbors()
+ifi_info = {}
+ifi_info[IFI] = NEIGHBOR
+def get_node_value():
+    rand = random.random()
+    num_neighbors = len(NEIGHBOR)
+    return num_neighbors*rand
+    
+asa, err = ASA_REG('asa')
+obj, err = OBJ_REG('node', [ifi_info, get_node_value()], True, False, 10, asa)
+tagged_node = TAG_OBJ(obj, asa)
+
+def discover_neighbors():
+    err, ll = graspi.discover(tagged_node.source,
+                            tagged_node.objective,
+                            10000,
+                            flush = False)
+    if (not err) and len(ll)> 0:
+        for item in ll:
+            if NEIGHBOR.__contains__(str(item.locator)):
+                NEIGHBOR_LOCATORS.append(item)
+print("*******")
+for item in NEIGHBOR_LOCATORS:
+    print("{}".format(str(item.locator)))
+print("*******")
+
+
+
+

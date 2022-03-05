32d770b0dc5c331e81adc95c9078f62cb183e1b4
Parsa <parsa@Parsas-MacBook-Air.local>
2022-03-05 15:27:24 -0500

    testing

--- test1.py
+++ test1.py
@@ -149,6 +149,10 @@ if sp.getoutput('hostname') == 'Tarjan':
     threading.Thread(target=listen, args=[tagged]).start()
     # threading.Thread(target=discover, args=[tagged]).start()
 
+if sp.getoutput('hostname') == 'Iverson':
+    tagged.objective.value = cbor.dumps(40)
+    threading.Thread(target=listen, args=[tagged]).start()
+    # threading.Thread(target=discover, args=[tagged]).start()
 
 if sp.getoutput('hostname') == 'Gingko':
     tagged.objective.value = cbor.dumps(30)

368bff5e928e93d1726143309b0812ac5aaf9d03
Parsa <parsa@Parsas-MacBook-Air.local>
2022-03-05 11:45:39 -0500

    testing

--- test1.py
+++ test1.py
@@ -112,14 +112,14 @@ def listener_handler(_tagged, _handle, _answer):
 
 
 def discover(_tagged):
-    attempt = 5
+    attempt = 20
     while attempt != 0:
         _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True)
         mprint(len(ll))
         for item in ll:
             mprint("asking {}".format(item.locator))
             # threading.Thread(target=neg, args=[_tagged, item]).start()
-            sleep(5)
+            sleep(1)
         attempt-=1
 
 def neg(_tagged, ll):

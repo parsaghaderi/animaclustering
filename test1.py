34f3bf93b6e243c3f68673dcbc7980fa3f12b569
Parsa <parsa@Parsas-MacBook-Air.local>
2022-03-06 10:23:54 -0500

    testing

--- test1.py
+++ test1.py
@@ -83,7 +83,7 @@ def gremlin():
         sleep(1)
 threading.Thread(target=gremlin, args=[]).start()
 
-node_info = {'weight':get_node_value(), 'cluster_head':False, 'cluster_set':[]}
+node_info = {'ula':str(acp._get_my_address()), 'weight':get_node_value(), 'cluster_head':False, 'cluster_set':[], 'neighbors':[]}
 obj, err = OBJ_REG('node', cbor.dumps(node_info), True, False, 10, asa)
 tagged   = TAG_OBJ(obj, asa)
 

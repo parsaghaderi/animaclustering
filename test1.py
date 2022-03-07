4834c3252b9ee170ea23ad92b5ef6d390fde4244
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-03-07 11:07:56 -0500

    testing

--- test1.py
+++ test1.py
@@ -277,7 +277,7 @@ def topo_discovery(_tagged):
         sleep(1)
     for item in NEIGHBOR_INFO.keys():
         mprint("asking item {}".format(str(item)))
-        threading.Thread(target = topo_request, args=[_tagged, item]).start()
+        # threading.Thread(target = topo_request, args=[_tagged, item]).start()
 threading.Thread(target=topo_discovery, args=[tagged]).start()
 def topo_request(_tagged, ll):
     mprint("asking {} for topo map".format(str(ll.locator)))

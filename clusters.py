330a5c358b556e280dd28ca4d18bc8d367d61b2b
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-04-01 12:35:32 -0400

    testing new grasp

--- clusters.py
+++ clusters.py
@@ -220,7 +220,7 @@ def neg(_tagged, ll, _attempt = 3):
                 NEIGHBOR_UPDATE[ll.locator] = True
             _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
         else:
-            mprint("neg failed + {}".format(graspi.etext[_err]))
+            mprint("neg failed + {}".format(graspi.etext[err]))
             err = graspi.end_negotiate(_tagged.source, handle, False, "value not received")
         sleep(3)
         attempt-=1

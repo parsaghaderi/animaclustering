6b8f2fad5cd41f9e985949a65450a9bfcbe873b6
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-04-20 15:55:43 -0400

    testing scenario II, stop previous listen and discovers

--- test.py
+++ test.py
@@ -108,14 +108,16 @@ def discover(_tagged, _attempt=5, _phase=0):
 
 
 threading.Thread(target=listen, args=[tagged_1]).start()
+
+
 if sp.getoutput('hostname') == 'Dijkstra':
     threading.Thread(target=listen, args=[tagged_1]).start()
-
     asa2, err = ASA_REG("test2")
     obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
     tagged_2 = TAG_OBJ(obj2, asa2)
     threading.Thread(target=listen, args=[tagged_2]).start()
     # # threading.Thread(target=listen, args=[tagged_1]).start()
+    sleep(10)
     threading.Thread(target=discover, args=[tagged_2]).start()
     
 

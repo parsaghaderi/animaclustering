590afc967556af606958613db173ee4ae945d25a
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-04-20 11:29:24 -0400

    testing scenario II, stop previous listen and discovers

--- test.py
+++ test.py
@@ -95,7 +95,7 @@ def discover(_tagged, _attempt=5, _phase=0):
     global FLAG
     attempt = _attempt
     while attempt != 0:
-        _, ll = graspi.discover(_tagged.source,_tagged.objective, 50000, flush=True, minimum_TTL=50000)
+        _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
         mprint(len(ll))
         sleep(1)
         attempt-=1
@@ -113,7 +113,7 @@ if sp.getoutput('hostname') == 'Dijkstra':
     obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
     tagged_2 = TAG_OBJ(obj2, asa2)
     # threading.Thread(target=listen, args=[tagged_2]).start()
-    threading.Thread(target=listen, args=[tagged_1]).start()
+    # threading.Thread(target=listen, args=[tagged_1]).start()
     threading.Thread(target=discover, args=[tagged_2]).start()
     
 
@@ -125,10 +125,10 @@ if sp.getoutput('hostname') == 'Gingko':
 if sp.getoutput('hostname') == 'Ritchie':
     threading.Thread(target=listen, args=[tagged_1]).start()
 
-    # asa2, err = ASA_REG("test2")
-    # obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
-    # tagged_2 = TAG_OBJ(obj2, asa2)
-    # threading.Thread(target=listen, args=[tagged_2]).start()
+    asa2, err = ASA_REG("test2")
+    obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
+    tagged_2 = TAG_OBJ(obj2, asa2)
+    threading.Thread(target=listen, args=[tagged_2]).start()
     
 
 if sp.getoutput('hostname') == 'Tarjan':
@@ -148,12 +148,12 @@ if sp.getoutput('hostname') == 'Iverson':
 
 
 if sp.getoutput('hostname') == 'Backus':
-    # while True:
-    #     pass
-    asa2, err = ASA_REG("test2")
-    obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
-    tagged_2 = TAG_OBJ(obj2, asa2)
-    threading.Thread(target=listen, args=[tagged_2]).start()
+    while True:
+        pass
+    # asa2, err = ASA_REG("test2")
+    # obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
+    # tagged_2 = TAG_OBJ(obj2, asa2)
+    # threading.Thread(target=listen, args=[tagged_2]).start()
     threading.Thread(target=listen, args=[tagged_1]).start()
 
     # threading.Thread(target=discover, args=[tagged_2]).start()

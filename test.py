a014db353826cc1a93e3fd1581aa6147d84790d8
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-04-20 12:33:14 -0400

    testing scenario II, stop previous listen and discovers

--- test.py
+++ test.py
@@ -153,19 +153,19 @@ if sp.getoutput('hostname') == 'Tarjan':
 
 if sp.getoutput('hostname') == 'Iverson':
     threading.Thread(target=listen, args=[tagged_1]).start()
-    # asa2, err = ASA_REG("test2")
-    # obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
-    # tagged_2 = TAG_OBJ(obj2, asa2)
-    # threading.Thread(target=listen, args=[tagged_2]).start()
+    asa2, err = ASA_REG("test2")
+    obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
+    tagged_2 = TAG_OBJ(obj2, asa2)
+    threading.Thread(target=listen, args=[tagged_2]).start()
 
 
 if sp.getoutput('hostname') == 'Backus':
 
     threading.Thread(target=listen, args=[tagged_1]).start()
-    asa2, err = ASA_REG("test2")
-    obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
-    tagged_2 = TAG_OBJ(obj2, asa2)
-    threading.Thread(target=listen, args=[tagged_2]).start()
+    # asa2, err = ASA_REG("test2")
+    # obj2, err = OBJ_REG("test_obj2", 20, True, False, 10, asa2)
+    # tagged_2 = TAG_OBJ(obj2, asa2)
+    # threading.Thread(target=listen, args=[tagged_2]).start()
     # threading.Thread(target=listen, args=[tagged_1]).start()
     # threading.Thread(target=discover, args=[tagged_2]).start()
 

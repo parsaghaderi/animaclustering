05add406da93cc5e74fe9515de60f0cc4337d27d
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-04-25 12:17:03 -0400

    testing failed node and cached info in middle nodes

--- brski_client.py
+++ brski_client.py
@@ -3,8 +3,6 @@ import threading
 import cbor
 import subprocess as sp
 from time import sleep
-
-from thread_safe_cluster import CLUSTER_HEAD
 try:
     import graspi
     _old_API = False    
@@ -91,16 +89,6 @@ node, err     = OBJ_REG("node", cbor.dumps(node_info), True,
 tagged_sem = threading.Semaphore()
 tagged       = TAG_OBJ(node, asa)
 
-
-
-server, err = OBJ_REG("server", None, True, False, 10, asa)
-tagged_server = TAG_OBJ(server, asa)
-
-# brski, err   = OBJ_REG("brski", None, True,
-#                     False, 10, asa)
-
-# tagged_brski = TAG_OBJ(brski, asa)
-
 #on all nodes
 def listen(_tagged):
     while True:
@@ -114,18 +102,6 @@ def listen(_tagged):
             mprint("\033[1;31;1m ERROR IN LISTEN - OBJ {} : {} \033[0m"
                     .format(_tagged.objective.name,graspi.etext[err]))           
 
-#on brski
-def listen_node_handler(_tagged, _handle, _answer):
-    tmp_answer = cbor.loads(_answer)
-    mprint("req_neg initial value : peer offered {}".format(tmp_answer))#√
-    for item in NEIGHBOR_INFO:#TODO can do better
-        if str(item.locator) == tmp_answer['ula']:
-            NEIGHBOR_INFO[item] = tmp_answer
-    #can send nothing and eng neg
-    _err = graspi.end_negotiate(_tagged.source, _handle, True, reason="value received")
-    if _err:
-        mprint("\033[1;31;1m in listen_node_handler error happened {} \033[0m".format(graspi.etext[_err]))
-
 def discover_neighbor(_tagged, _attempts = 3):
     attempt = _attempts
     while attempt!=0:
@@ -138,45 +114,6 @@ def discover_neighbor(_tagged, _attempts = 3):
                 LOCATOR_STR[str(item.locator)] = item.locator
                 NEIGHBOR_INFO[item.locator] = []
                 mprint("\033[1;32;1m new neighbor found {}\033[0m".format(str(item.locator)))
-
-def discovery_brski(_tagged, _attempts = 3):
-    global BRSKI_locator
-    attempt = _attempts
-    while attempt!=0:
-        _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=1)
-        if len(ll) != 0:
-            attempt -= 1
-    for item in ll:
-        BRSKI_locator = item.locator
-    mprint("\033[1;32;1m brski server found {}\033[0m".format(str(BRSKI_locator)))
-
-def neg(_tagged, ll, _attempts, _phase):
-    _try = 1
-    attempt = _attempts
-    while attempt != 0:
-        mprint("\033[1;32;1m in phase: {}, start negotiating with {} for {}th time - try {} \033[0m".format(_phase, ll.locator, attempt, _try))
-        if _old_API:
-            err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, 10000) #TODO
-            reason = answer
-        else:
-            err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
-        if err:
-            mprint("\033[1;31;1m in neg error happened {} \033[0m".format(graspi.etext[err]))
-        else:
-            mprint("\033[1;32;1m negotiation ended with reason {}\033[0m".format(reason))
-#on client
-def listen_brski_update(_tagged, _handle, _answer):
-    tmp_answer = cbor.loads(_answer.value)
-    ROLE = tmp_answer['role'] #cluster_head or ordinary node
-    CLUSTER_HEAD = tmp_answer['cluster'] #str, look at the str->locator dict 
-    if tmp_answer['role'] == "ch":#cluster_head or ordinary node
-        CLUSTER_SET = tmp_answer['cluster_set']
-    _err = graspi.end_negotiate(_tagged.source, _handle, True, reason="value received")
-    if _err:
-        mprint("\033[1;31;1m in listen_node_handler error happened {} \033[0m".format(graspi.etext[_err]))
-
-# def listen for updates to make changes and send to cluster head! clusterhead might change
-
 
 threading.Thread(target=listen, args=[tagged]).start()
 threading.Thread(target=discover_neighbor, args=[tagged]).start()

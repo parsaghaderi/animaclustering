cc0db458ccb392eb1d3da459c25836bfd382d317
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-06-09 13:00:11 -0400

    creating the library

--- new_test.py
+++ new_test.py
@@ -1,389 +1,118 @@
-import random
-import threading
-import cbor
-import subprocess as sp
-from time import sleep
+from clusters import INITIAL_NEG, TO_JOIN
+from test import TP_MAP
+from thread_safe_cluster import MAP_SEM
+from utility import *
+from utility import _old_API as _old_API
 
-# import grasp
-try:
-    import graspi
-    _old_API = False    
-except:
-    import grasp as graspi
-    _old_API = True
-import acp
+'''
+NEIGHBORS str(item.locator)
+NEIGHBOR_STR to stor ula addresses as string
+NEIGHBOR_DATA to store neighbor information
 
+HEAVIER to store heavier neighbors ()
+HEAVIEST to store the heaviest neighbor ()
+LIGHTER to store lighter neighbors ()
+'''
 
+NEIGHBORS_STR = []
+NEIGHBORS_LOCATOR_TO_STR = {}
+NEIGHBOR_INFO = {} #TODO change
 
+HEAVIER = {}
+LIGHTER = {}
+HEAVIEST = None
 
-def get_neighbors():
-    f = open('/etc/TD_neighbor/locators')
-    l = f.readlines()
-    l = [item.rstrip('\n') for item in l]
-    return l[0], l[1:]
+CLUSTER_HEAD = False
+INITIAL_NEG = False
+CLUSTERING_DONE = False
+SYNCH = False
+TO_JOIN = None
+CLUSTER_INFO = {}
+CLUSTER_INFO_KEYS = []
+CLUSTER_NODES = {}
 
-MY_ULA, NEIGHBOR_ULA = get_neighbors()
-NEIGHBOR_INFO = {}
-NEIGHBOR_UPDATE = {}
-locators = {}
-CH = None
-CH_locators = {}
-DONE = False #TODO change the name to something more meaningful
-# NEIGHBORING = {str(acp._get_my_address()):[]}
-#########################
-# utility function for setting the value of
-# each node randomly. 
-#########################
-def get_node_value():
-    return random.random()
-    
+TP_MAP = {}
+MAP_SEM = threading.Semaphore()
 
-#########################
-# utility print function
-#########################
-def mprint(msg):
-    print("\n#######################")
-    print(msg)
-    print("#######################\n")
-
-#########################
-#Registering ASA 
-#########################
-def ASA_REG(name):
-    mprint("registering asa and objective")
-    err, asa_handle = graspi.register_asa(name)
-    if not err:
-        mprint("ASA registered successfully")
-        return asa_handle, err
-    else:
-        mprint("Cannot register ASA:\n\t" + graspi.etext[err])
-        mprint("exiting now.")
+listen_sub = None
 
 
-#########################
-#Registering objectives
-#########################
-def OBJ_REG(name, value, neg, synch, loop_count, ASA):
-    obj = graspi.objective(name)
-    obj.value = value
-    obj.neg = neg
-    obj.synch = synch
-    obj.loop_count = loop_count
-    err = graspi.register_obj(ASA, obj)
-    if not err:
-        mprint("Objective registered successfully")
-    else:
-        mprint("Cannot register Objective:\n\t"+ graspi.etext[err])
-        mprint("exiting now.")
-    return obj, err
-        
-def TAG_OBJ(obj, ASA):
-    return graspi.tagged_objective(obj, ASA)
+'''
+# node_info['weight'] is run once, that's why we don't need a tmp variable to store node's weight
+# status 1:not decided, 2:cluster-head, 3:want to join, 4:joined 5:changed (!)
+'''
 asa, err = ASA_REG('node_neg')
-def gremlin():
-    while True:
-        sleep(1)
-threading.Thread(target=gremlin, args=[]).start()
+asa2, err = ASA_REG('cluster_neg')
 
-node_info = {'ula':str(acp._get_my_address()), 'weight':get_node_value(), 'cluster_head':False, 'cluster_set':[], 'neighbors':[]} #TODO didn't accept set
+node_info = {'ula':str(acp._get_my_address()), 'weight':get_node_value(),
+             'cluster_head':False, 'cluster_set':[], 'neighbors':NEIGHBORS_STR, 
+             'status': 1} 
+
 obj, err = OBJ_REG('node', cbor.dumps(node_info), True, False, 10, asa)
 tagged   = TAG_OBJ(obj, asa)
+tagged_sem = threading.Semaphore()
 
-def listen(_tagged):
-    global listen_semaphore
-    while True:
-        
-        err, handle, answer = graspi.listen_negotiate(_tagged.source, _tagged.objective)
-        if not err:
-            #mprint("incoming request")
-            threading.Thread(target=listener_handler, args=[_tagged, handle, answer]).start()
-        else:
-            mprint(graspi.etext[err])
+cluster_obj1, err = OBJ_REG("cluster_head", cbor.dumps(TP_MAP), True, False, 10, asa)
+cluster_tagged = TAG_OBJ(cluster_obj1, asa)
+cluster_tagged_sem = threading.Semaphore()
 
-def listener_handler(_tagged, _handle, _answer):
+sub_cluster_obj, err = OBJ_REG("sub_cluster", cbor.dumps(TP_MAP), True, False, 10, asa)
+sub_cluster_tagged = TAG_OBJ(sub_cluster_obj, asa)
+cluster_tagged_sem = threading.Semaphore()
+
+
+def listen_handler(_tagged, _handle, _answer):
+    initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
     tmp_answer = cbor.loads(_answer.value)
     mprint("req_neg initial value : peer offered {}".format(tmp_answer))#√
-    
-    mprint("sent from peer {}".format(tmp_answer))
-    #TODO get info from the answer
-    ###########
-    for item in NEIGHBOR_INFO:
-        if NEIGHBOR_INFO[item]['ula'] == tmp_answer['ula']:
+    for item in NEIGHBOR_INFO:#TODO just deleted
+        if str(item.locator) == str(ipaddress.IPv6Address(_handle.id_source)):
             NEIGHBOR_INFO[item] = tmp_answer
-            break
-    ############
-    _answer.value = _tagged.objective.value #TODO can be optimized by using the info in request (answer)
-    _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-    if _old_API:
-        err, temp, answer = _r
-        reason = answer
-    else:
-        err, temp, answer, reason = _r
-    if (not err) and (temp == None):
-        pass
-        
-    else:
-        mprint("neg with peer interrupted with error code {}".format(graspi.etext[err]))
-        pass
-
-def run_neg(_tagged,ll):
-    global DONE
-    attempt = 5
-    while attempt!=0:
-        threading.Thread(target=neg, args=[_tagged, ll]).start()
-        attempt-=1
-    DONE = True
-
-def discover(_tagged):
-    attempt = 3
-    while attempt != 0:
-        _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
-        mprint(len(ll))
-        attempt-=1
-    for item in ll:
-        if not node_info['neighbors'].__contains__(str(item.locator)): #cause timeout in locators are different! adding without checking the address causes duplicate values
-            node_info['neighbors'].append(str(item.locator))
-            NEIGHBOR_UPDATE[item.locator] = False
-            locators[item.locator] = item
-    _tagged.objective.value = cbor.dumps(node_info)
-    for item in ll:
-        # mprint(item.locator) #√
-        threading.Thread(target=neg, args=[_tagged, item]).start()
-        # threading.Thread(target=run_neg, args=[_tagged, item]).start()
-def neg(_tagged, ll, _attempt = 3):
-    global DONE
-    
-    if _attempt!=3:
-        mprint("start negotiation o kire khar {}".format(ll.locator))
-    else:
-        mprint("start negotiating with {}".format(ll.locator))
-    global NEIGHBOR_INFO
-    NEIGHBOR_INFO[ll] = 0 # initial neg, later it's just updates
-    attempt = _attempt
-    while attempt!=0:
+            if node_info['cluster_set'].__contains__(tmp_answer['ula']):
+                mprint("*\n&\n*\n&\n*\n&\n*\n&\n*\n&\n*\n&\n")
+    tagged_sem.acquire()
+    _answer.value = _tagged.objective.value #TODO can be optimized by using the info in request (answer) - just deleted
+    tagged_sem.release()
+    try:
+        _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
         if _old_API:
-            err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, None) #TODO
+            err, temp, answer = _r
             reason = answer
         else:
-            err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
-        if not err:
-            
-            NEIGHBOR_INFO[ll] = cbor.loads(answer.value)#√
-            # mprint("neg_step value : peer {} offered {}".format(str(ll.locator), NEIGHBOR_INFO[ll]))#√
-            if NEIGHBOR_INFO[ll]['cluster_head'] == str(acp._get_my_address()): #√
-                if not node_info['cluster_set'].__contains__(str(ll.locator)):
-                    node_info['cluster_set'].append(str(ll.locator))
-                _tagged.objective.value = cbor.dumps(node_info)
-                NEIGHBOR_UPDATE[ll.locator] = True
-            _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
+            err, temp, answer, reason = _r
+        if (not err) and (temp == None):
+            pass
         else:
-            mprint("neg failed + {}".format(graspi.etext[_err]))
-            _err = graspi.end_negotiate(_tagged.source, handle, False, "value not received")
-        sleep(3)
-        attempt-=1
-    DONE = True
-        
-
-threading.Thread(target=listen, args=[tagged]).start()
-threading.Thread(target=discover, args=[tagged]).start()
+            mprint("\033[1;31;1m in listen handler - neg with peer interrupted with error code {} \033[0m".format(graspi.etext[err]))
+            pass
+    except Exception as err:
+        mprint("\033[1;31;1m exception in linsten handler {} \033[0m".format(err))
 
 
-HEAVIER = []
-HEAVIEST = False
-HEAVY_UPDATE = {}
-# cluster_obj, err = OBJ_REG('clustering', cbor.dumps({CLUSTER_HEAD:CLUSTER_SET}))
-# tagged_clustering = TAG_OBJ(cluster_obj, asa)
-def find_heavier():
-    global DONE
-    while not DONE:
-        pass
-    global HEAVIEST,HEAVIER
-    tmp = {}
-    max_weight = cbor.loads(obj.value)['weight']
-    max_key = False
-    for item in NEIGHBOR_INFO:
-        mprint("peer {} has weight {}".format(str(item.locator), NEIGHBOR_INFO[item]['weight']))
-        if NEIGHBOR_INFO[item]['weight'] > cbor.loads(obj.value)['weight']:
-            # HEAVIER.append(item)
-            tmp[item] = NEIGHBOR_INFO[item]['weight']
-            if  NEIGHBOR_INFO[item]['weight'] > max_weight:
-                max_weight = NEIGHBOR_INFO[item]['weight']
-                max_key = item
-    HEAVIEST = max_key
-    tmp_sorted = dict(sorted(tmp.items(), key=lambda item: item[1], reverse = True))
-    for item in tmp_sorted.keys():
-        HEAVIER.append(item)
-    if HEAVIEST == False:
-        mprint("the value of my heaviest is {}".format(HEAVIEST))
-    else:
-        mprint("the value of my heaviest is {}".format(str(HEAVIEST.locator)))
-    mprint("$$$$$$$$$$$$$$${}$$$$$$$$$$$$$$$$".format(HEAVY_UPDATE))
-    if HEAVIEST == False:
-        mprint("the value of my heaviest is {}".format(HEAVIEST))
-    else:
-        mprint("the value of my heaviest is {}".format(str(HEAVIEST.locator)))
+def discovery_node_handler(_tagged, _locators):
+    for item in _locators:
+        if item not in NEIGHBORS_LOCATOR_TO_STR:
+            NEIGHBORS_LOCATOR_TO_STR[item] = str(item.locator)
+            NEIGHBORS_STR.append(str(item.locator))
+            NEIGHBOR_INFO[item] = 0
+            tagged_sem.acquire()
+            _tagged.objective.value = cbor.dumps(node_info)
+            tagged_sem.release()
+    mprint(NEIGHBORS_STR)
+    threading.Thread(target=run_neg, args=[tagged, NEIGHBOR_INFO.keys(), 1]).start()
 
-def run_neg_update():
-    global NEIGHBOR_INFO
-    for item in NEIGHBOR_INFO.keys():
-        threading.Thread(target=neg, args=[tagged, item, 1]).start()
-INIT_CH = False
-def init():
-    global NEIGHBOR_ULA
-    global DONE
-    global HEAVIER,HEAVIEST
-    while not DONE:
-        pass
-    # while len(NEIGHBOR_INFO) != len(NEIGHBOR_ULA): #TODO check if we actually need this. we have DONE
-    #     sleep(2)
-    find_heavier()
-    if HEAVIEST == False:
-        mprint("I'm cluster head")
-        node_info['cluster_head'] = True
-        node_info['cluster_set'].append(str(acp._get_my_address()))
-        tagged.objective.value = cbor.dumps(node_info)
-        
-    else:
-        mprint("want to join {}".format(str(HEAVIEST.locator)))
-    threading.Thread(target=run_neg_update, args=[]).start()
-    sleep(5*len(NEIGHBOR_ULA))
-    sleep(10)
-
-    #TODO do the negotiation to get the cluster heads once more
-
-    #threading.Thread(target=on_update_rcv, args=[]).start()
-
-threading.Thread(target=init, args=[]).start()
-
-def on_update_rcv():
-    global NEIGHBOR_INFO
-    joined = False
-    if node_info['cluster_head'] == False:
-        for item in HEAVIER:
-            # mprint("{}".format(NEIGHBOR_INFO[item]))
-            # if node_info['cluster_head'] == str(item) and NEIGHBOR_INFO[item]['cluster_head'] != True:
-            #     mprint("cluster head joined another cluster {}, should start looking for a new cluster head".format(NEIGHBOR_INFO[item]['cluster_head']))
-            if NEIGHBOR_INFO[item]['cluster_head'] == True:
-                mprint("joining {}".format(item.locator))
-                joined = True
-                node_info['cluster_head'] = str(item.locator)
-                node_info['cluster_set'] = []
-                tagged.objective.value = cbor.dumps(node_info)
-                CH = item
-                break
-            
-        if not joined:
-            mprint("I'm cluster head - from on-update_rcv")
-            node_info['cluster_head'] = True
-            node_info['cluster_set'].append(str(acp._get_my_address()))
-            tagged.objective.value = cbor.dumps(node_info)
-    sleep(5*len(NEIGHBOR_ULA))
-    threading.Thread(target=run_neg_update, args=[]).start()
+def discovery_cluster_handler(_tagged, _locators):
+    for item in _locators:
+        if str(item.locator) != MY_ULA:
+            CLUSTER_INFO[str(item.locator)] = 0
+            CLUSTER_INFO_KEYS.append(item)
+            mprint("cluster head found at {}".format(str(item.locator)))
+    threading.Thread(target=run_clustering_neg, args=[_tagged, CLUSTER_INFO_KEYS, 1]).start()
 
 
-# def keep_track():#check ch status from heavier nodes
-#     #while in init stop
-    
 
-# threading.Thread(target=keep_track, args=[]).start()
+listen_1 = threading.Thread(target=listen, args=[tagged, listen_handler]) #TODO change the name
+listen_1.start()
 
-# ch_obj, err = OBJ_REG('ch', None, True, False, 10, asa)
-# tagged_ch   = TAG_OBJ(ch_obj, asa)
-# ch_lock = False
-
-
-# def generate_topology(): #call after discovery
-#     mprint("generating the topology")
-#     global ch_lock
-#     while ch_lock:
-#         pass
-#     ch_lock = True#semaphore
-#     topology = {}
-#     topology[str(acp._get_my_address())] = node_info['neighbors']
-#     for item in NEIGHBOR_INFO:
-#         if NEIGHBOR_INFO[item]['cluster_head'] == str(acp._get_my_address()):
-#             topology[NEIGHBOR_INFO[item]['ula']] = NEIGHBOR_INFO[item]['neighbors']
-    
-#     tagged_ch.objective.value = topology #take care of it in listen handler
-#     ch_lock = False
-#     mprint("topology created {}".format(topology))
-# def CH_neg(_tagged, ll):
-#     mprint("sending ch-neg message to {}".format(str(ll.locator)))
-#     while True:
-#         global ch_lock
-#         while ch_lock:
-#             pass
-#         ch_lock = True
-#         tagged_ch.objective.value = cbor.dumps(tagged_ch.objective.value)
-#         if _old_API:
-#             err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, None) #TODO
-#             reason = answer
-#         else:
-#             err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
-#         if not err:
-#             tmp_map = cbor.loads(answer.value)
-#             tagged_ch.objective.value = cbor.loads(tagged_ch.objective.value)
-#             tagged_ch.objective.value.update(tmp_map)
-#             mprint("updated map {}".format(tagged_ch.objective.value))
-#             # tagged_ch.objective.value = cbor.dumps(tagged_ch.objective.value) #take care of it somewhere else
-
-#             _err = graspi.end_negotiate(_tagged.source, handle, True, reason = "map updated")
-#             ch_lock = False
-#         sleep(5) #todo change dynamically
-
-# def CH_listen_handler(_tagged, _handle, _answer):
-#     mprint("handling incoming request from another ch")
-#     global ch_lock
-#     while ch_lock:
-#         pass
-#     ch_lock = True
-#     # tagged_ch.objective.value = cbor.dumps(tagged_ch.objective.value)
-#     _answer.value = cbor.loads(_answer.value)
-#     tagged_ch.objective.value.update(_answer.value)
-#     mprint("updated map on listen-handler {}".format(tagged_ch.objective.value))
-#     mprint("map updated {}".format(tagged_ch.objective.value))
-#     _answer.value = cbor.dumps(tagged_ch.objective.value)
-#     _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-#     ch_lock = False
-#     if _old_API:
-#         err, temp, answer = _r
-#         reason = answer
-#     else:
-#         err, temp, answer, reason = _r
-#     if (not err) and (temp == None):
-#         pass
-#     else:
-#         mprint("neg with peer interrupted with error code {}".format(graspi.etext[err]))
-#         pass
-
-
-# def CH_discovery(_tagged):
-#     while node_info['cluster_head'] != True:
-#         sleep(2)
-#     attempt = 5
-#     while attempt!= 0: #TODO change, now the network is stable, so we can do it. later for dynamic networks
-#         _, ll = graspi.discover(_tagged.source, _tagged.objective, 10000, flush=True, minimum_TTL=50000)
-#         for item in ll:
-#             if not CH_locators.keys().__contains__(item.locator):
-#                 CH_locators[item.locator] = item
-
-#         sleep(3)
-#         attempt-=1
-#     mprint("{} cluster heads found")
-#     generate_topology()
-#     sleep(10) #TODO to reach stability, change later
-#     for item in CH_locators:
-#         threading.Thread(target=CH_neg, args=[_tagged, CH_locators[item]]).start()
-
-# def CH_listen(_tagged):
-#     while node_info['cluster_head'] != True:
-#         sleep(2)
-#     while True:
-#         err, handle, answer = graspi.listen_negotiate(_tagged.source, _tagged.objective)
-#         if not err:
-#             threading.Thread(target=CH_listen_handler, args=[_tagged, handle, answer]).start()
-#         else:
-#             mprint(graspi.etext[err])
-
-# threading.Thread(target=CH_listen,    args=[tagged_ch]).start()
-# threading.Thread(target=CH_discovery, args=[tagged_ch]).start()
+discovery_1 = threading.Thread(target=discovery, args=[tagged,discovery_node_handler, 2])
+discovery_1.start()

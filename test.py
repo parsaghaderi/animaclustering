647c56ba2efc3a5f8484d1826a7f091f37eb6bb1
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-06-09 11:20:33 -0400

    checking type of handle

--- test.py
+++ test.py
@@ -68,7 +68,7 @@ cluster_tagged_sem = threading.Semaphore()
 
 def listen_handler(_tagged, _handle, _answer):
     tmp = _handle.id_source
-    mprint("*_*_*_*_*_*_*_*_*_*_*\n{}\n*_*_*_*_*_*_*_*_*_*".format(tmp.unpack))
+    mprint("*_*_*_*_*_*_*_*_*_*_*\n{}\n*_*_*_*_*_*_*_*_*_*".format(tmp.unpacked))
     tmp_answer = cbor.loads(_answer.value)
     mprint("req_neg initial value : peer offered {}".format(tmp_answer))#√
     for item in NEIGHBOR_INFO:#TODO just deleted

159bdd8956a9dafc48ebd3c5d50b7847a638ea79
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-07-11 12:06:17 -0400

    making a 2-way thing

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ listen_sub = None
 asa, err = ASA_REG('node_neg')
 asa2, err = ASA_REG('cluster_neg')
 
-mprint(asa)
+mprint(len(graspi._session_id_cache))
 
 
 # node_info = {'ula':str(acp._get_my_address()), 'weight':get_node_value(),

72b378efd3f9e89e8d039bdcf20a0cd91267744e
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-07-25 11:52:46 -0400

    initial brski commit

--- brski.py
+++ brski.py
@@ -2,37 +2,46 @@ from utility import *
 from utility import _old_API as _old_API
 import sys
 
-REGISTRAR = sys.argv[1] == "True"
-REGISTRAR_LOCATOR = None
-PROXY_LOCATOR = None
-
+list_of_approved = []
 
 asa, err  = ASA_REG('brski')
 
-obj, err = OBJ_REG('brski_obj', False, True, False, 10, asa)
-tagged_obj = TAG_OBJ(obj, asa)
+pledge, err = OBJ_REG('pledge', True, True, False, 10, asa)
+pledge_tagged = TAG_OBJ(pledge, asa)
 
-def discovery_proxy(_tagged):
-    global PROXY_LOCATOR
-    mprint("discoverying proxy")
-    _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
-    mprint("proxy locator found at {}".format(str(ll[0].locator)), 2)
-    PROXY_LOCATOR = ll[0]
+registrar, err = OBJ_REG('registrar', True, True, False, 10, asa)
+registrar_tagged = TAG_OBJ(registrar, asa)
 
-def listen_proxy_req(_tagged, _listen_handler=None):
-    mprint("start listening for objective {}".format(_tagged.objective.name))
-    while True:
-        err, handle, answer = graspi.listen_negotiate(_tagged.source, 
-                                                      _tagged.objective)       
-        if not err:
-            initiator_ula = str(ipaddress.IPv6Address(handle.id_source))
-            mprint("\033[1;32;1m incoming request from {}\033[0m".format())
-            if REGISTRAR:
-                mprint("From Registrar")
-            else:
-                mprint("From Proxy")
+# proxy, err = OBJ_REG('brski_proxy', False, True, False, 10, asa)
+# proxy_tagged = TAG_OBJ(proxy, asa)
 
-threading.Thread(target=listen_proxy_req, args=[tagged_obj]).start()
-if not REGISTRAR:
-    threading.Thread(target=discovery_proxy, args=[tagged_obj]).start()
 
+def listen_proxy_handler(_tagged, _handle, _answer):
+    global list_of_approved
+    initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
+    mprint("\033[1;32;1m incoming request from {}\033[0m".format())
+    tmp_answer = cbor.loads(_answer.value)
+    if True:
+        mprint("contacting MASA with the Pledge's certificate")
+        list_of_approved.append(initiator_ula)
+        mprint(list_of_approved)
+    try:
+        _answer.value = cbor.dumps(True)
+        _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
+        if _old_API:
+            err, temp, answer = _r
+            reason = answer
+        else:
+            err, temp, answer, reason = _r
+        if (not err) and (temp == None):
+            mprint("\033[1;32;1m negotiation with peer {} ended successfully \033[0m".format(initiator_ula))  
+        else:
+            mprint("\033[1;31;1m in listen handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]))
+            pass
+    except Exception as err:
+        mprint("\033[1;31;1m exception in linsten handler {} \033[0m".format(err))
+
+def listen_registrar_handler(_tagged, _handle, _answer):
+    pass
+
+threading.Thread(target=listen, args=[pledge_tagged, listen_proxy_handler]).start()
---
+++ brski_pledge.py
@@ -0,0 +1,63 @@
+from cmath import phase
+from utility import *
+from utility import _old_API as _old_API
+import sys
+
+REGISTRAR_LOCATOR = None
+PROXY_LOCATOR = None
+
+PROXY_STATE = False
+
+asa, err  = ASA_REG('brski')
+
+pledge, err = OBJ_REG('pledge', False, True, False, 10, asa)
+pledge_tagged = TAG_OBJ(pledge, asa)
+
+def discovery_proxy(_tagged):
+    global PROXY_LOCATOR
+    mprint("discoverying proxy")
+    _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
+    mprint("proxy locator found at {}".format(str(ll[0].locator)), 2)
+    PROXY_LOCATOR = ll[0]
+    threading.Thread(target=neg_with_proxy, args=[_tagged, PROXY_LOCATOR]).start()
+
+def discovery_registrar(_tagged): 
+    global PROXY_STATE
+    if PROXY_STATE:
+        mprint("looking for registrar")
+
+def neg_with_proxy(_tagged, ll):
+    global PROXY_STATE
+    mprint("negotiating with REGISTRAR")
+    try:
+        if _old_API:
+            err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, 10000) #TODO
+            reason = answer
+        else:
+            err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
+
+        if not err:
+            if cbor.loads(answer) == True:
+                mprint("can join network - key stored for further comm - ACP booted up")
+                PROXY_STATE = True
+                _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
+                return answer
+        else:
+            mprint("Registrar didn't respond")
+            _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
+            return False
+    except Exception as e:
+        mprint("there was an error occurred in neg_REGISTRAR with code {}".format(graspi.etext[e]))
+
+# def proxy_listen_handler(_tagged, _handle, _answer):
+
+
+
+def init_proxy(): #listener
+    global PROXY_STATE
+    while not PROXY_STATE:
+        sleep(5)
+    mprint("acting as proxy")
+    
+
+threading.Thread(target=discovery_proxy, args=[pledge_tagged]).start()

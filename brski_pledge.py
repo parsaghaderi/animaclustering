399c38b7dd3028ab2734ebdc0de9716eab9115b7
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-07-26 11:51:11 -0400

    dull false

--- brski_pledge.py
+++ brski_pledge.py
@@ -28,7 +28,7 @@ def discovery_proxy(_tagged):
         mprint("discoverying proxy", 2)
         _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
         if len(ll) != 0:
-            mprint("proxy locator found at {}".format(ll), 2)
+            mprint("proxy locator found at {}".format(str(ll.locator), 2)
             # PROXY_LOCATOR = ll[0]
             mprint("start negotiation with proxy", 2)
             PHASE = 2

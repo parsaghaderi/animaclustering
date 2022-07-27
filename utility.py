b29ac6f7eff1b8aa39284e49f2fd710cd2b08025
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-07-27 14:37:37 -0400

    adding gitignore file

--- utility.py
+++ utility.py
@@ -61,7 +61,7 @@ def OBJ_REG(name, value, neg, synch, loop_count, ASA, _local = False):
     err = graspi.register_obj(ASA, obj, local=_local)
     if not err:
         mprint("Objective registered successfully")
-        for item in graspi.graspi._obj_registry:
+        for item in graspi.grasp._obj_registry:
             if item.objective.name == name:
                 mprint("$%#\n{}\n#$%".format(item.port),2)
 

928d5c69a78a021a2c0774d3741f5b89cf4f0326
Parsa <parsa@parsas-air.wireless.concordia.ca>
2022-07-28 11:32:39 -0400

    testing neg with pledge from registrar

--- brski.py
+++ brski.py
@@ -2,167 +2,52 @@ from grasp import asa_locator
 from utility import *
 from utility import _old_API as _old_API
 import random
-# MAP = {'MAP':{MY_ULA:NEIGHBOR_ULA}, 'PORTS':{'pledge':0, 'registrar':0, 'proxy':0}}
 
-# list_of_approved = []
+MAP = {MY_ULA:NEIGHBOR_ULA} #the whole map of the network, only map
+NETWORK_INFO = {} #what info was received from each node
 
-# asa, err  = ASA_REG('brski')
+nodes_locator = {}
 
-# pledge, err, pledge_port = OBJ_REG('pledge', cbor.dumps(MAP), True, False, 10, asa)
-# pledge_tagged = TAG_OBJ(pledge, asa)
-# pledge_sem = threading.Semaphore()
+node_info = {'MAP':MAP, 'PORTS':PORTS} #my info
 
-# registrar, err, registrar_port = OBJ_REG('registrar', cbor.dumps(MAP), True, False, 10, asa)
-# registrar_tagged = TAG_OBJ(registrar, asa)
-# registrar_sem = threading.Semaphore()
+asa, err = ASA_REG('brski')
 
-# proxy, err, proxy_port = OBJ_REG('proxy', cbor.dumps(MAP), True, False, 10, asa, True)
-# proxy_tagged = TAG_OBJ(proxy, asa)
-# proxy_sem = threading.Semaphore()
-# # proxy, err = OBJ_REG('brski_proxy', False, True, False, 10, asa)
-# # proxy_tagged = TAG_OBJ(proxy, asa)
+proxy_obj, err, proxy_port = OBJ_REG('proxy', None, True, False, 10, asa, True) #for pledges and communication only
+proxy_tagged = TAG_OBJ(proxy_obj,asa)
+proxy_sem = threading.Semaphore()
+
+registrar_obj, err, registrar_port = OBJ_REG('registrar', None, True, False, 10, asa, False) #for transferring updates
+registrar_tagged = TAG_OBJ(registrar_obj, asa)
+registrar_sem = threading.Semaphore()
+
+pledge_obj, err, pledge_port = OBJ_REG()#for communication with pledge only
+pledge_tagged = TAG_OBJ(pledge_obj, asa)
+pledge_sem = threading.Semaphore()
 
 
-# def listen_proxy_handler(_tagged, _handle, _answer):
-#     global list_of_approved, pledge_tagged, proxy_tagged
-#     initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-#     mprint("***\n{}\n***".format(type(_handle)), 2)
-#     mprint("\033[1;32;1m incoming request from {}\033[0m".format(initiator_ula), 2)
-#     tmp_answer = cbor.loads(_answer.value)
-#     if True:
-#         mprint("contacting MASA with the Pledge's certificate", 2)
-#         list_of_approved.append(initiator_ula)
-#         mprint(list_of_approved)
-
-#     try:
-#         if random.randint(0, 10)%3 != 0:
-#             proxy_sem.acquire()
-#             registrar_sem.acquire()
-#             pledge_sem.acquire()
-#             # MAP['MAP'].update(tmp_answer['MAP'])
-#             _tagged.objective.value = cbor.dumps(tmp_answer)
-#             proxy_tagged.objective.value = cbor.dumps(MAP)
-#             pledge_tagged.objective.value = cbor.dumps(MAP)
-#             _answer.value = cbor.dumps(MAP)
-#             proxy_sem.release()
-#             registrar_sem.release()
-#             pledge_sem.release()
-#         else:
-#             _answer.value = cbor.dumps(False)
-#         mprint(tmp_answer, 2)
-#         _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-#         if _old_API:
-#             err, temp, answer = _r
-#             reason = answer
-#         else:
-#             err, temp, answer, reason = _r
-#         if (not err) and (temp == None):
-#             mprint("\033[1;32;1m negotiation with peer {} ended successfully with value {}\033[0m".format(initiator_ula, cbor.loads(_answer.value)), 2)  
-#             mprint("trying to contact pledge", 2)
-#             sleep(10)
-#             threading.Thread(target=neg_with_pledge, args=[registrar_tagged,ipaddress.IPv6Address(list(tmp_answer['MAP'].keys())[0]),tmp_answer['PORTS']['registrar']]).start()
-#         else:
-#             mprint("\033[1;31;1m in listen handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
-#             pass
-#     except Exception as err:
-#         mprint("\033[1;31;1m exception in linsten handler {} \033[0m".format(err), 2)
-
-# def listen_registrar_handler(_tagged, _handle, _answer):
-#     global MAP
-#     initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-#     mprint("\033[1;32;1m incoming request from {}\033[0m".format(initiator_ula), 2)
-#     tmp_answer = cbor.loads(_answer.value)
-#     # MAP[initiator_ula] = tmp_answer
-#     # registrar_sem.acquire()
-
-#     proxy_sem.acquire()
-#     registrar_sem.acquire()
-#     pledge_sem.acquire()
-#     MAP.update(tmp_answer)
-#     _tagged.objective.value = cbor.dumps(MAP)
-#     proxy_tagged.objective.value = cbor.dumps(MAP)
-#     pledge_tagged.objective.value = cbor.dumps(MAP)
-#     _answer.value = cbor.dumps(MAP)
-#     proxy_sem.release()
-#     registrar_sem.release()
-#     pledge_sem.release()
-
-#     _answer.value = cbor.dumps(MAP)
-#     registrar_tagged.objective.value = cbor.dumps(MAP)
-#     registrar_sem.release()
-#     try:
-#         _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-#         if _old_API:
-#                 err, temp, answer = _r
-#                 reason = answer
-#         else:
-#             err, temp, answer, reason = _r
-#         if (not err) and (temp == None):
-#             mprint("\033[1;32;1m negotiation with peer {} ended successfully \033[0m".format(initiator_ula), 2)  
-        
-#         else:
-#             mprint("\033[1;31;1m in listen handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
-#             pass
-#     except Exception as err:
-#         mprint("\033[1;31;1m exception in linsten handler {} \033[0m".format(err), 2)
-        
-
-# def neg_with_pledge(_tagged, _ll, _port):
+def listen_proxy(_tagged, _handle, _answer): #to join pledge
     
-#     tmp_locator = graspi.asa_locator(_ll, 0, False)
-#     tmp_locator.port = _port
+    proxy_address = str(ipaddress.IPv6Address(_handle.id_source))
+    tmp_answer = cbor.loads(_answer.value) #{map:..., ports:....}
+    actual_initiator_ula = list(tmp_answer['MAP'].keys())[0]
+    mprint("\033[1;32;1m incoming request from {}\033[0m".format(actual_initiator_ula), 2)
 
-#     try:
-#         if _old_API:
-#             err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, tmp_locator, 10000) #TODO
-#             reason = answer
-#         else:
-#             err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, tmp_locator, None)
+    if (random.randint(0, 10)%4 != 0):
+        mprint("connecting to MASA", 2)
+        sleep(2)
+        mprint("MASA approved")
+        proxy_sem.acquire()
+        registrar_sem.acquire()
+        MAP.update(tmp_answer['MAP'])
+        nodes_locator[actual_initiator_ula] = tmp_answer['PORTS']
+        _answer.value = cbor.dumps(node_info)
+        proxy_sem.release()
+        registrar_sem.release()
+        
+    else:
+        mprint("Rejecting pledge")
+        _answer.value = cbor.dumps(False)
 
-#         if not err:
-#             mprint("communicating with the pledge with answer {}".format(cbor.loads(answer.value)), 2)
-#             _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-            
-#     except Exception as e:
-#         mprint("there was an error occurred in neg_with_registrar with code {}".format(graspi.etext[e]), 2)
-
-
-# threading.Thread(target=listen, args=[proxy_tagged, listen_proxy_handler]).start()
-# threading.Thread(target=listen, args=[registrar_tagged, listen_registrar_handler]).start()
-
-
-
-asa, err = ASA_REG('asa')
-_value = {'ula':MY_ULA, 'port':0, 'value':10}
-obj, err, obj_port = OBJ_REG('obj', None, True, False, 10, asa, True)
-_value['port'] = obj_port
-obj.value = cbor.dumps(_value)
-obj_tagged = TAG_OBJ(obj, asa)
-reply = None
-reply2 = None
-
-def negotiate_obj(_tagged, _ll):
-    try:
-        if _old_API:
-            err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, _ll, 10000) #TODO
-            reason = answer
-        else:
-            err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, _ll, None)
-
-        if not err:
-            mprint("the offered value is {}".format(cbor.loads(answer.value)),2)
-            _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-        else:
-            mprint("error occurred!", 2)
-    except Exception as e:
-        mprint("exception happended at negotiation {}".format(graspi.etext[e]), 2)
-
-
-def listen_handler(_tagged, _handle, _answer):
-    global reply, reply2
-    initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-    mprint("incoming request from {}".format(initiator_ula), 2)
-    reply = cbor.loads(_answer.value)
     try:
         _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
         if _old_API:
@@ -171,25 +56,16 @@ def listen_handler(_tagged, _handle, _answer):
         else:
             err, temp, answer, reason = _r
         if (not err) and (temp == None):
-            mprint("\033[1;32;1m negotiation with peer {} ended successfully \033[0m".format(initiator_ula), 2)  
-            sleep(10)
-            mprint("starting negotiation with peer without discovery", 2)
-            ll = locator_maker(reply['ula'], int(reply['port']), False)
-            threading.Thread(target=negotiate_obj, args=[_tagged, ll]).start()
-
+            mprint("\033[1;32;1m negotiation with peer {} with proxy {} ended successfully with value {}\033[0m".format(actual_initiator_ula, proxy_address,cbor.loads(_answer.value)), 2)  
         else:
-            mprint("\033[1;31;1m in listen_handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
+            mprint("\033[1;31;1m in listen handler - neg with peer {} with proxy {} interrupted with error code {} \033[0m".format(actual_initiator_ula, proxy_address, graspi.etext[err]), 2)
             pass
     except Exception as err:
-        mprint("\033[1;31;1m exception in listen_handler {} \033[0m".format(err), 2)
+        mprint("\033[1;31;1m exception in linsten handler {} \033[0m".format(err), 2)
+    
+threading.Thread(target=listen, args = [proxy_tagged, listen_proxy]).start()
 
 
+def listen_registrar(_tagged, _handle, _answer):#to get updates from nodes
+    pass
 
-# def locator_maker(_ip, _port, _diverted):
-#     locator_obj = asa_locator(ipaddress.IPv6Address(_ip),0, _diverted)
-#     locator_obj.port = _port
-
-#     return locator_obj
-
-
-threading.Thread(target=listen, args=[obj_tagged, listen_handler]).start()
--- brski_pledge.py
+++ brski_pledge.py
@@ -1,310 +1,50 @@
 from utility import *
 from utility import _old_API as _old_API
 import ipaddress
-from grasp import asa_locator
-# REGISTRAR_LOCATOR = None
-# PROXY_LOCATOR = None
 
-# PROXY_STATE = False
-# PHASE = 1
+MAP = {MY_ULA:NEIGHBOR_ULA}
 
-# MAP = {'MAP': {MY_ULA:NEIGHBOR_ULA}, 'PORTS':{'pledge':0, 'registrar':0, 'proxy':0}}
-# asa, err  = ASA_REG('brski')
+REGISTRAR_LOCATOR = None
+PROXY_LOCATOR = None
 
-# pledge, err, pledge_port = OBJ_REG('pledge', cbor.dumps(MAP), True, False, 10, asa)
-# MAP['PORTS']['pledge'] = pledge_port
-# pledge_tagged = TAG_OBJ(pledge, asa)
+NODE_INFO = {}
+REGISTRAR_UPDATES = {}
+PORTS = {'proxy':0, 'registrar':0}
+node_info = {'MAP':MAP, 'PORTS':PORTS}
 
-# pledge_sem = threading.Semaphore()
+asa, err = ASA_REG('brski')
 
-# registrar, err, registrar_port = OBJ_REG('registrar', cbor.dumps(MAP), True, False, 10, asa)
-# MAP['PORTS']['registrar'] = registrar_port
-# registrar_tagged = TAG_OBJ(registrar, asa)
-# registrar_sem = threading.Semaphore()
-# mprint("!@#\n{}\n!@#".format(registrar_port), 2)
-# proxy, err, proxy_port = OBJ_REG('proxy', cbor.dumps(MAP), True, False, 10, asa, True)
-# MAP['PORTS']['proxy'] = proxy_port
-# proxy_tagged = TAG_OBJ(proxy, asa)
-# proxy_sem = threading.Semaphore()
+proxy_obj, err, PORTS['PORTS']['proxy'] = OBJ_REG('proxy', None, True, False, 10, asa, True) #for pledges and communication only
+proxy_tagged = TAG_OBJ(proxy_obj,asa)
+proxy_sem = threading.Semaphore()
 
-# pledge_tagged.objective.value = cbor.dumps(MAP)
-# registrar_tagged.objective.value = cbor.dumps(MAP)
+registrar_obj, err, PORTS['PORTS']['registrar'] = OBJ_REG('registrar', None, True, False, 10, asa, False) #for transferring updates
+registrar_tagged = TAG_OBJ(registrar_obj, asa)
+registrar_sem = threading.Semaphore()
 
+proxy_tagged.objective.value = cbor.dumps(node_info)
+registrar_tagged.objective.value = cbor.dumps(node_info)
 
-# def discovery_proxy(_tagged):
-#     while True:
-#         global PROXY_LOCATOR, PHASE
-#         mprint("discoverying proxy", 2)
-#         _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
-#         if len(ll) != 0:
-#             mprint("proxy locator found at {} on interface {}".format(str(ll[0].locator), str(ll[0].ifi)), 2)
-#             PROXY_LOCATOR = ll[0]
-#             mprint("!!!!!!!!!!!!\n{}\n!!!!!!!!!!!!!!!!".format(ll[0].diverted), 2)
-#             mprint("start negotiation with proxy", 2)
-#             PHASE = 2
-#             break
-#         else:
-#             sleep(10)
-    
-#     # threading.Thread(target=neg_with_proxy, args=[_tagged, PROXY_LOCATOR]).start()
+pledge_obj, err, pledge_port = OBJ_REG('pledge', None, True, False, 10, asa, True)#for communication with pledge only
+pledge_tagged = TAG_OBJ(pledge_obj, asa)
+pledge_sem = threading.Semaphore()
 
-# def discovery_registrar(_tagged): 
-#     global REGISTRAR_LOCATOR, PHASE
-#     while True:
-#         mprint("discoverying registrar", 2)
-#         _, ll =  graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
-#         if len(ll)!= 0:
-#             mprint("Registrar found at {}".format(str(ll[0].port)), 2)
-#             REGISTRAR_LOCATOR = ll[0]
-#             threading.Thread(target=listen, args=[proxy_tagged, proxy_listen_handler]).start() #to communicate with registrar
-#             # threading.Thread(target=listen, args=[pledge_tagged, pledge_listen_handler]).start() #to update registred nodes
-#             threading.Thread(target=listen, args=[registrar_tagged, registrar_update_listen_handler]).start()
-#             sleep(5)
-#             mprint("entering phase 4",2)
-#             PHASE = 5
-#             return   
-#         else:
-#             sleep(5)
-#     # threading.Thread(target=listen, args=[proxy_tagged, proxy_listen_handler]).start() #to communicate with registrar
-#     # threading.Thread(target=listen, args=[pledge_tagged, pledge_listen_handler]).start() #to update registred nodes
-#     # sleep(5)
-#     # PHASE = 4
-    
-
-# def neg_with_proxy(_tagged, ll):
-#     global PROXY_STATE, registrar_tagged, PHASE
-#     mprint("negotiating with proxy", 2)
-#     mprint("***\n{}\n***".format(ll.port),2)
-#     try:
-#         if _old_API:
-#             err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, 10000) #TODO
-#             reason = answer
-#         else:
-#             err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
-
-#         if not err:
-#             if cbor.loads(answer.value) != False:
-#                 mprint("can join network - key stored for further comm - ACP booted up", 2)
-                
-#                 registrar_sem.acquire()
-#                 pledge_sem.acquire()
-#                 registrar_tagged.objective.value = cbor.loads(answer.value)
-#                 pledge_tagged.objective.value = cbor.loads(answer.value)
-#                 registrar_sem.release()
-#                 pledge_sem.release()
-#                 PROXY_STATE = True
-
-#                 _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-#                 mprint("looking for the registrar", 2)
-#                 PHASE = 3
-#                 # discovery_registrar_thread = threading.Thread(target=discovery_registrar, args=[registrar_tagged])
-#                 # discovery_registrar_thread.start()
-#                 # discovery_registrar_thread.join()
-#                 # threading.Thread(target=listen, args=[proxy_tagged, proxy_listen_handler]).start()
-#             else:
-#                 mprint("Registrar rejected Pledge or there is problem in communication with Registrar")
-#                 _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-#                 sleep(20)
-#                 PHASE = 1
-#         else:
-#             mprint("Proxy didn't respond")
-#             # _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-#             sleep(20)
-#             PHASE = 1
-            
-#     except Exception as e:
-#         mprint("there was an error occurred in neg_with_proxy with code {}".format(graspi.etext[e]), 2)
-#         PHASE = 1
-        
-
-# def neg_with_registrar(_tagged, ll):
-#     global PHASE
-#     mprint("negotiating with registrar", 2)
-#     mprint("***\n{}\n***".format(ll.port),2)
-#     try:
-#         if _old_API:
-#             err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, ll, 10000) #TODO
-#             reason = answer
-#         else:
-#             err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, ll, None)
-        
-#         if not err:
-#             # if cbor.loads(answer.value) == True:
-#             mprint("communicating with the registrar with answer {}".format(cbor.loads(answer.value)), 2)
-#             _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-#             PHASE = 5
-#         # else:
-#         #     mprint("Registrar didn't respond", 2)
-#         #     _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-#         #     PHASE = 5
-#     except Exception as e:
-#         mprint("there was an error occurred in neg_with_registrar with code {}".format(graspi.etext[e]), 2)
-
-# def relay(_answer):
-#     global proxy_tagged
-#     mprint("relaying voucher request")
-#     proxy_sem.acquire()
-#     proxy_tagged.objective.value = cbor.dumps(_answer)
-    
-#     try:
-#         if _old_API:
-#                 err, handle, answer = graspi.req_negotiate(proxy_tagged.source,proxy_tagged.objective, REGISTRAR_LOCATOR, 10000) #TODO
-#                 reason = answer
-#         else:
-#             err, handle, answer, reason = graspi.request_negotiate(proxy_tagged.source,proxy_tagged.objective, REGISTRAR_LOCATOR, None)
-#         proxy_sem.release()
-#         if not err:
-#             mprint("got voucher response form registrar", 2)
-#             _err = graspi.end_negotiate(proxy_tagged.source, handle, True, reason="value received")
-#             proxy_sem.release()
-#             return cbor.loads(answer.value)
-#     except Exception as e:
-#         mprint("exception experienced during relay negotiation process with code {}".format(graspi.etext[e]), 2)
-#         return cbor.dumps(False)
-
-# # def proxy_listen_handler(_tagged, _handle, _answer):
-
-# def proxy_listen_handler(_tagged, _handle, _answer):
-#     initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-#     _answer.value = cbor.dumps(relay(cbor.loads(_answer.value)))#TODO
-
-#     try:
-#         _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-#         if _old_API:
-#             err, temp, answer = _r
-#             reason = answer
-#         else:
-#             err, temp, answer, reason = _r
-#         if (not err) and (temp == None):
-#             mprint("\033[1;32;1m negotiation with pledge {} ended successfully \033[0m".format(initiator_ula), 2)  
-#         else:
-#             mprint("\033[1;31;1m in proxy_listen_handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
-#             pass
-#     except Exception as err:
-#         mprint("\033[1;31;1m exception in proxy_listen_handler {} \033[0m".format(err), 2)
-
-# def pledge_listen_handler(_tagged, _handle, _answer):
-#     mprint("waiting for updates from registrar", 2)
-
-# def registrar_update_listen_handler(_tagged, _handle, _answer):
-#     initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-#     mprint("\033[1;32;1m incoming request from {}\033[0m".format(initiator_ula), 2)
-#     tmp_answer = cbor.loads(_answer.value)
-#     MAP['MAP'].update(tmp_answer['MAP'])
-#     try:
-#         _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-#         if _old_API:
-#             err, temp, answer = _r
-#             reason = answer
-#         else:
-#             err, temp, answer, reason = _r
-#         if (not err) and (temp == None):
-#             mprint("\033[1;32;1m negotiation with pledge {} ended successfully \033[0m".format(initiator_ula), 2)  
-#         else:
-#             mprint("\033[1;31;1m in proxy_listen_handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
-#             pass
-#     except Exception as e:
-#         mprint("\033[1;31;1m exception in proxy_listen_handler {} \033[0m".format(err), 2)
-
-
-# def control():
-#     global PHASE
-#     while True:
-#         if PHASE == 1:
-#             discovery_proxy_thread = threading.Thread(target=discovery_proxy, args=[proxy_tagged])
-#             discovery_proxy_thread.start()
-#             discovery_proxy_thread.join()
-#         elif PHASE == 2:
-#             neg_with_proxy_thread = threading.Thread(target=neg_with_proxy, args=[proxy_tagged, PROXY_LOCATOR])
-#             neg_with_proxy_thread.start()
-#             neg_with_proxy_thread.join()
-#         elif PHASE == 3:
-#             discovery_registrar_thread = threading.Thread(target=discovery_registrar, args=[registrar_tagged])
-#             discovery_registrar_thread.start()
-#             discovery_registrar_thread.join()
-#         elif PHASE == 4:
-#             mprint("calling neg with registrar", 2)
-#             neg_with_registrar_thread = threading.Thread(target=neg_with_registrar, args=[registrar_tagged, REGISTRAR_LOCATOR])
-#             neg_with_registrar_thread.start()
-#             neg_with_registrar_thread.join()
-#         elif PHASE == 5:
-#             pass
-#         sleep(1)
-# threading.Thread(target=control, args = []).start()
-
-
-
-
-
-
-asa, err = ASA_REG('asa')
-_value = {'ula':MY_ULA, 'port':0, 'value':15}
-obj, err, obj_port = OBJ_REG('obj', None, True, False, 10, asa, True)
-_value['port'] = obj_port
-obj.value = cbor.dumps(_value)
-obj_tagged = TAG_OBJ(obj, asa)
-reply = None
-reply2 = None
-
-def negotiate_obj(_tagged, _ll):
-    try:
-        if _old_API:
-            err, handle, answer = graspi.req_negotiate(_tagged.source,_tagged.objective, _ll, 10000) #TODO
-            reason = answer
-        else:
-            err, handle, answer, reason = graspi.request_negotiate(_tagged.source,_tagged.objective, _ll, None)
-
-        if not err:
-            mprint("the offered value is {}".format(cbor.loads(answer.value)),2)
-            _err = graspi.end_negotiate(_tagged.source, handle, True, reason="value received")
-            sleep(5)
-            threading.Thread(target=listen, args=[_tagged, listen_handler]).start()
-        else:
-            mprint("error occurred!", 2)
-
-    except Exception as e:
-        mprint("exception happended at negotiation {}".format(graspi.etext[e]), 2)
-
-
-def listen_handler(_tagged, _handle, _answer):
-    initiator_ula = str(ipaddress.IPv6Address(_handle.id_source))
-    mprint("incoming request from {}".format(initiator_ula), 2)
-
-    try:
-        _r = graspi.negotiate_step(_tagged.source, _handle, _answer, 10000)
-        if _old_API:
-            err, temp, answer = _r
-            reason = answer
-        else:
-            err, temp, answer, reason = _r
-        if (not err) and (temp == None):
-            mprint("\033[1;32;1m negotiation with peer {} ended successfully \033[0m".format(initiator_ula), 2)  
-        else:
-            mprint("\033[1;31;1m in listen_handler - neg with peer {} interrupted with error code {} \033[0m".format(initiator_ula, graspi.etext[err]), 2)
-            pass
-    except Exception as err:
-        mprint("\033[1;31;1m exception in listen_handler {} \033[0m".format(err), 2)
-
-
-
-def locator_maker(_ip, _port, _diverted):
-    locator_obj = asa_locator(ipaddress.IPv6Address(_ip),0, _diverted)
-    locator_obj.port = _port
-
-    return locator_obj
-
-def discover(_tagged):
-    while True:
-        mprint("discoverying obj", 2)
+def discover_proxy(_tagged):
+    global PROXY_LOCATOR
+    for i in range(0, 3):
         _, ll = graspi.discover(_tagged.source,_tagged.objective, 10000, flush=True, minimum_TTL=50000)
         if len(ll) != 0:
-            mprint("proxy locator found at {}\n{}\n{}\n{}".format(str(ll[0].locator), str(ll[0].ifi), str(ll[0].port), str(ll[0].diverted)), 2)
-            sleep(5)
-            threading.Thread(target=negotiate_obj, args=[_tagged, ll[0]]).start()
-            break
+            mprint("proxy found at {}".format(str(ll[0].locator)), 2)
+            PROXY_LOCATOR = ll[0]
+            return
         else:
-            sleep(10)
+            mprint("trying one more time")
+            sleep(5)
+    
 
+def send_voucher_req(_tagged, _proxy):
+    pass
 
-threading.Thread(target=discover, args=[obj_tagged]).start()
+def discover_registrar(_tagged): #it doesn't pass the proxy since proxy has already cached the locator
+    pass
 
